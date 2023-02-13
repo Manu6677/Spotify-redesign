ec29c194c9a3db0e5fbb0120aace6e4a1a28a90d
manupanjoria <manu.panjoria@bonamisoftware.com>
2023-02-13 20:32:02 +0530

    first commit

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,8 +1,28 @@
 import Image from "next/image";
+import { AiFillHome, AiFillCompass, AiFillClockCircle } from "react-icons/ai";
+import { FaMicrophoneAlt } from "react-icons/fa";
+import { BsFillBarChartFill, BsThreeDots } from "react-icons/bs";
+
 function Sidebar() {
   return (
-    <section>
-      <Image src="https://rb.gy/xkacau" width={56} height={56} />
+    <section
+      className="fixed top-0 z-40 flex flex-col p-4 items-center
+     bg-black w-90 h-screen space-y-8"
+    >
+      <Image
+        src="https://rb.gy/xkacau"
+        width={56}
+        height={56}
+        objectFit="contain"
+      />
+      <div className="space-y-8">
+        <AiFillHome className="sidebarIcon text-white" />
+        <AiFillCompass className="sidebarIcon" />
+        <FaMicrophoneAlt className="sidebarIcon" />
+        <AiFillClockCircle className="sidebarIcon" />
+        <BsFillBarChartFill className="sidebarIcon" />
+        <BsThreeDots className="sidebarIcon" />
+      </div>
     </section>
   );
 }
